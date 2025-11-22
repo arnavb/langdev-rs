eeb05868644c7a1fb6af13fdb9702ecb82417eec
Arnav Borborah <arnavborborah1@gmail.com>
2025-11-21 22:39:11 -0500

    Try implementing a trait based version of parser combinators
    This is closely modeled after how Iterator is implemented in the Rust
    standard library. Different combinators get their own structs, all of
    which implement the Parser trait, that can be evaluated at the end.

---
+++ pc/src/closure_based.rs
@@ -0,0 +1,417 @@
+use std::fmt::{Debug, Display};
+
+#[derive(Debug)]
+pub struct ParseError {
+    message: String,
+}
+
+impl ParseError {
+    fn new(message: &str) -> Self {
+        ParseError {
+            message: message.to_owned(),
+        }
+    }
+}
+
+fn expect_specific<T: Display>(expected: &T, actual: &T) -> ParseError {
+    ParseError::new(&format!("Expected '{}', got '{}'", expected, actual))
+}
+
+fn expect<T: Display>(expected: &T, actual: &str) -> ParseError {
+    ParseError::new(&format!("Expected '{}', got {}", expected, actual))
+}
+
+fn nothing() -> ParseError {
+    ParseError::new("Expected anything, got nothing")
+}
+
+/// A parser takes an input (e.g a string), and turns it into a parsed token or error and
+/// remaining input. We implement it as a subtrait as function aliases would require a type alias
+/// for impl Trait's, an unstable feature. Creating this subtrait and then implementing it for all
+/// blanket types effectively accomplishes the same thing.
+pub trait Parser<T>: Fn(&str) -> (Result<T, ParseError>, &str) {}
+
+impl<T, F> Parser<T> for F where F: Fn(&str) -> (Result<T, ParseError>, &str) {}
+
+/// Match any character
+pub fn any_char(input: &str) -> (Result<char, ParseError>, &str) {
+    match input.chars().next() {
+        Some(ch) => (Ok(ch), &input[ch.len_utf8()..]),
+        None => (Err(nothing()), input),
+    }
+}
+
+/// Match a specific character
+/// (could be implemented in terms of any_char(), but that doesn't add much
+pub fn char_parser(to_match: char) -> impl Parser<char> {
+    move |input| match input.chars().next() {
+        Some(ch) if ch == to_match => (Ok(ch), &input[to_match.len_utf8()..]),
+        Some(ch) => (Err(expect_specific(&to_match, &ch)), input),
+        None => (Err(expect(&to_match, "nothing")), input),
+    }
+}
+
+/// Combine two parsers in succession, and return a tuple of their combined results. If either
+/// parser fails, then the corresponding input is not consumed and the error is returned.
+pub fn and_then<T, U>(parser1: impl Parser<T>, parser2: impl Parser<U>) -> impl Parser<(T, U)> {
+    move |input| {
+        let (result1, remaining1) = parser1(input);
+
+        match result1 {
+            Ok(token1) => {
+                let (result2, remaining2) = parser2(remaining1);
+
+                match result2 {
+                    Ok(token2) => (Ok((token1, token2)), remaining2),
+                    // Don't consume second input if second parser errors
+                    Err(err2) => (Err(err2), remaining1),
+                }
+            }
+            // Don't consume input if first parser errors
+            Err(err1) => (Err(err1), input),
+        }
+    }
+}
+
+/// Run either of two parsers of the same type. If the first succeeds, return its result.
+/// Otherwise return the result of the second parser. If any parser fails, input should not be
+/// consumed with respect to that parser.
+pub fn or_else<T>(parser1: impl Parser<T>, parser2: impl Parser<T>) -> impl Parser<T> {
+    move |input| {
+        let (result1, remaining1) = parser1(input);
+
+        match result1 {
+            Ok(token1) => (Ok(token1), remaining1),
+            Err(_) => parser2(remaining1),
+        }
+    }
+}
+
+/// Implement choice, which is essentially equivalent to folding over or_else: keep trying parsers
+/// in a collection until one works, and if none do, return the last result. Not actually
+/// implemented using or_else, because this is difficult to statically do with Rust and how traits
+/// work (they end up being different opaque types, which can't be reduced).
+pub fn choice<T>(parsers: &[impl Parser<T>]) -> impl Parser<T> {
+    move |input| {
+        let [first, rest_parsers @ ..] = parsers else {
+            return (Err(nothing()), input);
+        };
+
+        // Will store successive error, and return the last one if no parser
+        // succeeds
+        let (mut result, remaining) = first(input);
+
+        if result.is_ok() {
+            return (result, remaining);
+        }
+
+        for parser in rest_parsers {
+            result = match parser(input) {
+                (Ok(token), remaining) => return (Ok(token), remaining),
+                (Err(err), _) => Err(err),
+            }
+        }
+
+        // If we reached here, no parser succeeded -> No input was consumed
+        (result, input)
+    }
+}
+
+/// Apply a mapping function from A -> B for a successful parser, otherwise propagate the parsing
+/// error
+pub fn map<A, B>(parser: impl Parser<A>, func: impl Fn(A) -> B) -> impl Parser<B> {
+    move |input| match parser(input) {
+        (Ok(token), rest) => (Ok(func(token)), rest),
+        (Err(err), rest) => (Err(err), rest),
+    }
+}
+
+/// Takes a normal value, and lifts (wraps) it into a parser (not consuming input)
+/// Can't move T into an Fn, so just as a hack will copy for now
+pub fn lift<T: Copy>(value: T) -> impl Parser<T> {
+    move |input| (Ok(value), input)
+}
+
+/// map, but at the parser level. Takes a parser that produces a function, and a parser that
+/// produces a value of the first argument type, and applies said function and wraps that value
+pub fn apply<A, B>(
+    func_parser: impl Parser<fn(A) -> B>, // Can be function pointer since no captures /
+    // lifetimes
+    value_parser: impl Parser<A>,
+) -> impl Parser<B> {
+    move |input| {
+        let func_and_value_parser = and_then(&func_parser, &value_parser);
+
+        map(func_and_value_parser, |(func, value)| func(value))(input)
+    }
+}
+
+// Lift two parameter functions
+pub fn lift2<A, B, C>(
+    func: fn(A, B) -> C, // Can be function pointer since no captures / lifetimes
+    parser1: impl Parser<A>,
+    parser2: impl Parser<B>,
+) -> impl Parser<C> {
+    move |input| unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn any_char_should_parse_any_character() {
+        let (token, rest) = any_char("a");
+
+        assert!(matches!(token, Ok('a')));
+        assert_eq!(rest, "");
+    }
+
+    #[test]
+    fn when_passed_nothing_any_char_should_error() {
+        let (token, rest) = any_char("");
+
+        assert!(token.is_err());
+        assert_eq!(rest, "");
+    }
+
+    #[test]
+    fn char_parser_should_return_a_parser() {
+        let a_parser = char_parser('a');
+
+        assert!(matches!(a_parser("abc"), (Ok('a'), "bc")));
+    }
+
+    #[test]
+    fn parse_char_can_be_called_multiple_times() {
+        let a_parser = char_parser('a');
+
+        let input = "aac";
+
+        let (token, rest) = a_parser(input);
+
+        assert!(matches!(token, Ok('a')));
+
+        let (token, rest) = a_parser(rest);
+
+        assert!(matches!(token, Ok('a')));
+
+        assert_eq!(rest, "c");
+    }
+
+    #[test]
+    fn parse_non_matching_should_return_error() {
+        let a_parser = char_parser('a');
+
+        let (token, rest) = a_parser("zyx");
+
+        assert!(token.is_err());
+        assert_eq!(rest, "zyx");
+    }
+
+    #[test]
+    fn parse_empty_string_should_return_error() {
+        let a_parser = char_parser('a');
+
+        let (token, rest) = a_parser("");
+
+        assert!(token.is_err());
+        assert_eq!(rest, "");
+    }
+
+    #[test]
+    fn and_then_should_parse_two_in_succession() {
+        let parser_a = char_parser('a');
+        let parser_b = char_parser('b');
+
+        let a_and_then_b = and_then(&parser_a, &parser_b);
+
+        let (result, rest) = a_and_then_b("abc");
+
+        assert!(matches!(result, Ok(('a', 'b'))));
+        assert_eq!(rest, "c");
+    }
+
+    #[test]
+    fn if_one_parser_fails_then_and_then_should_error() {
+        let parser_a = char_parser('a');
+        let parser_b = char_parser('b');
+
+        let a_and_then_b = and_then(&parser_a, &parser_b);
+
+        // First parser fails
+        let (result, rest) = a_and_then_b("zbc");
+
+        assert!(result.is_err());
+        assert_eq!(rest, "zbc");
+
+        // Second parser fails
+        let (result, rest) = a_and_then_b("azc");
+
+        assert!(result.is_err());
+        assert_eq!(rest, "zc");
+    }
+
+    #[test]
+    fn or_else_should_parse_either_of_two_parsers() {
+        let parser_a = char_parser('a');
+        let parser_b = char_parser('b');
+
+        let a_or_else_b = or_else(&parser_a, &parser_b);
+
+        let (result, rest) = a_or_else_b("a");
+
+        assert!(matches!(result, Ok('a')));
+        assert_eq!(rest, "");
+
+        let (result, rest) = a_or_else_b("b");
+
+        assert!(matches!(result, Ok('b')));
+        assert_eq!(rest, "");
+    }
+
+    #[test]
+    fn if_both_parsers_fail_then_or_else_should_error() {
+        let parser_a = char_parser('a');
+        let parser_b = char_parser('b');
+
+        let a_or_else_b = or_else(&parser_a, &parser_b);
+
+        let (result, rest) = a_or_else_b("c");
+
+        assert!(result.is_err());
+        assert_eq!(rest, "c");
+    }
+
+    #[test]
+    fn should_be_able_to_combine_and_then_and_or_else() {
+        let parser_a = char_parser('a');
+        let parser_b = char_parser('b');
+        let parser_c = char_parser('c');
+
+        let b_or_else_c = or_else(&parser_b, &parser_c);
+        let a_and_then_b_or_else_c = and_then(&parser_a, &b_or_else_c);
+
+        let (result, rest) = a_and_then_b_or_else_c("abz");
+
+        assert!(matches!(result, Ok(('a', 'b'))));
+        assert_eq!(rest, "z");
+
+        let (result, rest) = a_and_then_b_or_else_c("acz");
+
+        assert!(matches!(result, Ok(('a', 'c'))));
+        assert_eq!(rest, "z");
+
+        let (result, rest) = a_and_then_b_or_else_c("zbz");
+
+        assert!(result.is_err());
+        assert_eq!(rest, "zbz");
+
+        println!("{:?}", result);
+
+        let (result, rest) = a_and_then_b_or_else_c("azz");
+
+        assert!(result.is_err());
+        assert_eq!(rest, "zz");
+
+        // TODO: Better error messages (e.g show that it was B OR C that was expected, not just C
+        println!("{:?}", result);
+    }
+
+    #[test]
+    fn if_given_list_of_parser_choice_should_try_each_one() {
+        let lowercase_parsers = ('a'..='z').map(char_parser).collect::<Vec<_>>();
+        let any_lowercase = choice(&lowercase_parsers);
+
+        for letter in 'a'..='z' {
+            let input = &format!("{letter}random");
+            let (result, rest) = any_lowercase(input);
+
+            assert!(matches!(result, Ok(l) if letter == l));
+            assert_eq!(rest, "random");
+        }
+    }
+
+    #[test]
+    fn if_given_list_parser_choice_and_none_match_should_return_error() {
+        // Exclude z
+        let lowercase_parsers = ('a'..='y').map(char_parser).collect::<Vec<_>>();
+        let any_lowercase = choice(&lowercase_parsers);
+
+        let input = &format!("zrandom");
+        let (result, rest) = any_lowercase(input);
+
+        assert!(result.is_err());
+        assert_eq!(rest, "zrandom");
+    }
+
+    #[test]
+    fn if_given_heterogenous_parsers_with_same_return_should_be_able_to_choice_over_them() {
+        // Exclude z
+        let lowercase_parsers = ('a'..='y').map(char_parser).collect::<Vec<_>>();
+
+        // It's not really possible to represent this as a closure directly, because of
+        // the lack of higher ranked trait bounds in order to represent that the returned
+        // string slice must live as long as the input. We can write a function to
+        // constrain, e.g like https://stackoverflow.com/a/46198877/6525260
+        let other_parser: fn(&str) -> (Result<char, ParseError>, &str) = |input| (Ok('z'), input);
+
+        let mut combined_list = lowercase_parsers
+            .iter()
+            .map(|p| p as &dyn Parser<char>)
+            .collect::<Vec<_>>();
+
+        combined_list.push(&other_parser);
+
+        // This should parse any character outside a..=y as z
+        let combined_parser = choice(&combined_list);
+
+        let (token, rest) = combined_parser("$");
+
+        assert!(matches!(token, Ok('z')));
+        assert_eq!(rest, "$");
+    }
+
+    #[test]
+    fn map_parser_converts_parsers() {
+        let digit_char_parsers_vec = ('0'..='9').map(char_parser).collect::<Vec<_>>();
+        let digit_char_parser = choice(&digit_char_parsers_vec);
+
+        // unwrap is safe here since the digit should've been filtered by the parser
+        let digit_parser = map(digit_char_parser, |ch| ch.to_digit(10).unwrap());
+
+        let (token, rest) = digit_parser("1ab");
+
+        assert!(matches!(token, Ok(1)));
+        assert_eq!(rest, "ab");
+    }
+
+    #[test]
+    fn map_parser_returns_an_error_if_parser_failed() {
+        let digit_char_parsers_vec = ('0'..='9').map(char_parser).collect::<Vec<_>>();
+        let digit_char_parser = choice(&digit_char_parsers_vec);
+
+        // unwrap is safe here since the digit should've been filtered by the parser
+        let digit_parser = map(digit_char_parser, |ch| ch.to_digit(10).unwrap());
+
+        let (token, rest) = digit_parser("ab");
+
+        assert!(token.is_err());
+        assert_eq!(rest, "ab");
+    }
+
+    #[test]
+    fn lift_can_lift_values_into_parsers() {
+        let value = 35;
+
+        let lifted_value = lift(value);
+
+        let (token, rest) = lifted_value("abc");
+
+        assert!(matches!(token, Ok(35)));
+        assert_eq!(rest, "abc");
+    }
+
+    #[test]
+    fn apply_allows_lifting_of_two_param_function() {}
+}
---
+++ pc/src/trait_based.rs
@@ -0,0 +1,214 @@
+/// Model after Iterator as a design, similar to how map, fold, and all the various other
+/// iterator methods use intermediary structs to model their desired functionality.
+/// Design idea:
+///
+/// Parser has an associated Output type
+/// Parser has a parse method --> Returns a ParseResult
+///
+/// Also has various chaining methods. These methods all return other structs that implement
+/// Parser. The final parse method call is the one that will actually evaluate the parser.
+pub trait Parser {
+    type Output;
+
+    /// Generic parse method from input to the output type specified
+    fn parse<'a>(&self, input: &'a str) -> ParseResult<'a, Self::Output>;
+
+    fn or_else<T>(self, other: T) -> OrElse<Self, T>
+    where
+        Self: Sized,
+        T: Parser<Output = Self::Output>,
+    {
+        OrElse {
+            parser_a: self,
+            parser_b: other,
+        }
+    }
+}
+
+/// Blanket implementation for all parser references
+impl<P: Parser> Parser for &P {
+    type Output = P::Output;
+
+    fn parse<'a>(&self, input: &'a str) -> ParseResult<'a, Self::Output> {
+        (**self).parse(input)
+    }
+}
+
+/// Is either the parsed Output type along with remaining input, or an error with the unconsumed
+/// input. Even though the input is present on both sides, modeling it in this way allows for
+/// easier chaining using Result methods.
+pub type ParseResult<'a, Output> = Result<(Output, &'a str), (ParseError, &'a str)>;
+
+/// Different parsing errors
+#[derive(Debug)]
+pub enum ParseError {
+    /// Expected end of input, but input was still present
+    ExpectedEofGotCharacter(char),
+
+    /// Expected a specific character, got end of input
+    ExpectedSpecificGotEof(char),
+
+    /// Expected a specific character, got something else specific
+    ExpectedSpecificGotSpecific(char, char),
+
+    /// Expected a general concept (e.g an identifier), but got end of input
+    ExpectedConceptGotEof(String),
+}
+
+pub struct Any;
+
+impl Parser for Any {
+    type Output = char;
+
+    fn parse<'a>(&self, input: &'a str) -> ParseResult<'a, Self::Output> {
+        match input.chars().next() {
+            Some(ch) => Ok((ch, &input[ch.len_utf8()..])),
+            None => Err((
+                ParseError::ExpectedConceptGotEof("a character".to_owned()),
+                input,
+            )),
+        }
+    }
+}
+
+pub struct Character(char);
+
+impl Parser for Character {
+    type Output = char;
+
+    fn parse<'a>(&self, input: &'a str) -> ParseResult<'a, Self::Output> {
+        match input.chars().next() {
+            Some(ch) if ch == self.0 => Ok((ch, &input[ch.len_utf8()..])),
+            Some(ch) => Err((ParseError::ExpectedSpecificGotSpecific(self.0, ch), input)),
+            None => Err((ParseError::ExpectedSpecificGotEof(self.0), input)),
+        }
+    }
+}
+
+pub struct Eof;
+
+impl Parser for Eof {
+    type Output = ();
+
+    fn parse<'a>(&self, input: &'a str) -> ParseResult<'a, Self::Output> {
+        match input.chars().next() {
+            Some(ch) => Err((ParseError::ExpectedEofGotCharacter(ch), input)),
+            None => Ok(((), input)),
+        }
+    }
+}
+
+pub struct OrElse<A, B> {
+    parser_a: A,
+    parser_b: B,
+}
+
+impl<A, B> Parser for OrElse<A, B>
+where
+    A: Parser,
+    B: Parser<Output = A::Output>,
+{
+    type Output = A::Output;
+
+    fn parse<'a>(&self, input: &'a str) -> ParseResult<'a, Self::Output> {
+        self.parser_a
+            .parse(input)
+            .or_else(|_| self.parser_b.parse(input))
+    }
+}
+
+/// Functional interface to the above structs/traits
+
+/// Evaluates a parser
+pub fn run_parser<'a, T: Parser>(parser: T, input: &'a str) -> ParseResult<'a, T::Output> {
+    parser.parse(input)
+}
+
+/// Parses any character
+pub fn any() -> Any {
+    Any
+}
+
+/// Parses a specific character
+pub fn character(ch: char) -> Character {
+    Character(ch)
+}
+
+/// Parses end of input
+pub fn eof() -> Eof {
+    Eof
+}
+
+/// Run one parser, if that results in an error, then return the result of the second parser
+pub fn or_else<A, B>(parser_a: A, parser_b: B) -> OrElse<A, B>
+where
+    A: Parser,
+    B: Parser<Output = A::Output>,
+{
+    OrElse { parser_a, parser_b }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_any() {
+        let (parsed, rest) = run_parser(any(), "input").unwrap();
+        assert_eq!(parsed, 'i');
+        assert_eq!(rest, "nput");
+
+        let (error, rest) = run_parser(any(), "").unwrap_err();
+        assert!(matches!(error, ParseError::ExpectedConceptGotEof(..)));
+        assert_eq!(rest, "");
+    }
+
+    #[test]
+    fn test_eof() {
+        let (parsed, rest) = run_parser(eof(), "").unwrap();
+        assert_eq!(parsed, ());
+        assert_eq!(rest, "");
+
+        let (error, rest) = run_parser(eof(), "a").unwrap_err();
+        assert!(matches!(error, ParseError::ExpectedEofGotCharacter('a')));
+        assert_eq!(rest, "a");
+    }
+
+    #[test]
+    fn test_character() {
+        let (parsed, rest) = run_parser(character('i'), "input").unwrap();
+        assert_eq!(parsed, 'i');
+        assert_eq!(rest, "nput");
+
+        let (error, rest) = run_parser(character('i'), "anput").unwrap_err();
+        assert!(matches!(
+            error,
+            ParseError::ExpectedSpecificGotSpecific('i', 'a')
+        ));
+        assert_eq!(rest, "anput");
+
+        let (error, rest) = run_parser(character('i'), "").unwrap_err();
+        assert!(matches!(error, ParseError::ExpectedSpecificGotEof(..)));
+        assert_eq!(rest, "");
+    }
+
+    #[test]
+    fn test_or_else() {
+        let parser = or_else(character('i'), character('a'));
+
+        let (parsed, rest) = run_parser(&parser, "input").unwrap();
+        assert_eq!(parsed, 'i');
+        assert_eq!(rest, "nput");
+
+        let (parsed, rest) = run_parser(&parser, "anput").unwrap();
+        assert_eq!(parsed, 'a');
+        assert_eq!(rest, "nput");
+
+        let (error, rest) = run_parser(&parser, "bnput").unwrap_err();
+        assert!(matches!(
+            error,
+            ParseError::ExpectedSpecificGotSpecific('a', 'b')
+        ));
+        assert_eq!(rest, "bnput");
+    }
+}
